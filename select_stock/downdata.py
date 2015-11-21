44466139deaf4548e83eb41496ea766b2af282b3
buptlishantao <buptlishantao@163.com>
2015-11-22 00:11:28 +0800

    update

--- select_stock/downdata.py
+++ select_stock/downdata.py
@@ -2,6 +2,40 @@ import sys
 sys.path.append("..")
 
 import tushare as ts
+import util
+import pandas
+
+class DownloadData(object):
+    """docstring for DownloadData"""
+    def __init__(self):
+        self.today = util.get_today_str()
+        self.begin_day = util.get_begin_day_str()
+
+    def get_stocks_id(self):
+        pass
 
 
+    def _get_stock_info(self):
+        pass
+
+
+    def _get_history_data(self):
+        pass
+
+    def _merge_to_history(self, sid):
+        pass
+
+
+    def download(self):
+        stocks_id = self.get_stocks_id()
+        for sid in stocks_id:
+            self._merge_to_history(sid)
+
+
+
+
+if __name__ == '__main__':
+    DL = DownloadData()
+    DL.download()
+        
 
